c8003a82d996e31ec58d69de28d8e5265236a4eb
Angelo Zammataro <juba.kem@gmail.com>
2022-08-05 00:03:30 +0200

    First Commit: Environments Example

---
+++ Environment-Example/src/main/java/co/develhope/Angelo/Environment/Example/EnvironmentExampleApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.Angelo.Environment.Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentExampleApplication.class, args);
+	}
+
+}
---
+++ Environment-Example/src/test/java/co/develhope/Angelo/Environment/Example/EnvironmentExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.Angelo.Environment.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
